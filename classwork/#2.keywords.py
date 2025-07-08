3a466c9a3cdc9febaf9a3bd63dd1529e6e4cbfea
Kodadinesh18 <kodadinesh18@gmail.com>
2025-07-08 17:30:24 +0530

    keywords

---
+++ classwork/#2.keywords.py
@@ -0,0 +1,7 @@
+import keyword as kw 
+# This code imports the keywords module and prints the list of Python keywords.
+# The keywords module provides a list of reserved words in Python that cannot be used as identifiers.   
+print(kw.kwlist)
+# Output: ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+# This output shows the list of reserved keywords in Python that cannot be used as variable names or identifiers.
+# These keywords are used to define the syntax and structure of the Python programming language.      
