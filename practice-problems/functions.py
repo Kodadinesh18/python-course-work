81985a55183cad76d99af41d25d5a182a3c2e7c6
Kodadinesh18 <kodadinesh18@gmail.com>
2025-08-04 09:23:16 +0530

    Dinesh Koda

---
+++ practice-problems/functions.py
@@ -0,0 +1,13 @@
+#practice problems on functions:
+
+'''1.add two numbers:
+
+def add(a,b):
+    return a+b
+
+print(add(2,4))'''
+
+#2.square a number:
+def square(n):
+    return n**2
+print(square(5))
